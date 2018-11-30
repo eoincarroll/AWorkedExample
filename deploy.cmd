703b777ade7acf7d430ee6d5c4941c69b91b634b
Eoin <eoin.carroll@gmail.com>
2018-11-30 16:14:36 +0000

    again

--- deploy.cmd
+++ deploy.cmd
@@ -9,9 +9,3 @@ REM ---Deploy the wwwroot folder in repository to default target (wwwroot)
 xcopy %DEPLOYMENT_SOURCE%\wwwroot\* %DEPLOYMENT_TARGET%/Y /s 
 
  
-
-REM ---Deploy the myapp folder in repository to folder above default target (wwwroot\..\myapp)
-
-xcopy %DEPLOYMENT_SOURCE%\myapp\* %DEPLOYMENT_TARGET%\..\myapp /Y /s 
-
- 
