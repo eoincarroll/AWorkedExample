a0200079bc6595c1ffebff553aa55641f7aa291c
Eoin <eoin.carroll@gmail.com>
2018-11-30 16:45:43 +0000

    and aigain

--- deploy.cmd
+++ deploy.cmd
@@ -2,10 +2,7 @@
 
 echo ---Deploying site 
 
- 
 
-REM ---Deploy the wwwroot folder in repository to default target (wwwroot)
-ren  %DEPLOYMENT_SOURCE%\site\wwwroot\AWorkedExample\target\*.war ROOT.war
-xcopy %DEPLOYMENT_SOURCE%\site\wwwroot\AWorkedExample\target\ROOT.war %DEPLOYMENT_TARGET%\webapps /Y /s 
+xcopy %DEPLOYMENT_SOURCE%\site\wwwroot\AWorkedExample\target\*.war %DEPLOYMENT_TARGET%\webapps /Y /s 
 
  
