4b7ce7246ba94ab00349d59e73994cfb27587c82
Eoin <eoin.carroll@gmail.com>
2018-11-30 16:21:25 +0000

    edit deploly script

--- deploy.cmd
+++ deploy.cmd
@@ -6,6 +6,6 @@ echo ---Deploying site
 
 REM ---Deploy the wwwroot folder in repository to default target (wwwroot)
 
-xcopy %DEPLOYMENT_SOURCE%\wwwroot\* %DEPLOYMENT_TARGET%/Y /s 
+xcopy %DEPLOYMENT_SOURCE%\wwwroot\AWorkedExample\target\ROOT.war %DEPLOYMENT_TARGET%\ROOT.war/Y /s 
 
  
